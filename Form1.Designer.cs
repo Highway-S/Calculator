655cc2dda4d849ee210bd5b2ad2df01adb1cf96d
Highway <phoorin.w@kkumail.com>
2024-12-17 12:27:39 +0700

    ส่งงาน

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            num1 = new TextBox();
+            num2 = new TextBox();
+            result = new TextBox();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(79, 63);
+            label1.Name = "label1";
+            label1.Size = new Size(40, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Num1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(79, 108);
+            label2.Name = "label2";
+            label2.Size = new Size(40, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Num2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(80, 152);
+            label3.Name = "label3";
+            label3.Size = new Size(39, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Result";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(251, 59);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "+";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // num1
+            // 
+            num1.Location = new Point(134, 59);
+            num1.Name = "num1";
+            num1.Size = new Size(100, 23);
+            num1.TabIndex = 4;
+            // 
+            // num2
+            // 
+            num2.Location = new Point(134, 105);
+            num2.Name = "num2";
+            num2.Size = new Size(100, 23);
+            num2.TabIndex = 5;
+            // 
+            // result
+            // 
+            result.Location = new Point(134, 149);
+            result.Name = "result";
+            result.Size = new Size(100, 23);
+            result.TabIndex = 6;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(251, 105);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 3;
+            button2.Text = "*";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(251, 149);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 3;
+            button3.Text = "Clear";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(352, 59);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 7;
+            button4.Text = "-";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(352, 104);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 8;
+            button5.Text = "/";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(result);
+            Controls.Add(num2);
+            Controls.Add(num1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private TextBox num1;
+        private TextBox num2;
+        private TextBox result;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //ข้อความตัวอักษร
+            string inputNum1 = num1.Text;
+            string inputNum2 = num2.Text;
+
+            // convert string to number (interger)
+            // int => interger เลขจำนวนเต็ม
+            // double / float => เลขทศนิยม
+            double iNum1 = double.Parse(inputNum1);
+            double iNum2 = double.Parse(inputNum2);
+
+            // int ทำให้เราสามารถทำการ + - * / ได้
+            double iResult = iNum1 + iNum2;
+
+            // ที่ตัวแปร result
+            //มีคุณสมบัตชื่อ Text
+            result.Text = iResult.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //ข้อความตัวอักษร
+            string inputNum1 = num1.Text;
+            string inputNum2 = num2.Text;
+
+            // convert string to number (interger)
+            // int => interger เลขจำนวนเต็ม
+            // double / float => เลขทศนิยม
+            double iNum1 = double.Parse(inputNum1);
+            double iNum2 = double.Parse(inputNum2);
+
+            // int ทำให้เราสามารถทำการ + - * / ได้
+            double iResult = iNum1 - iNum2;
+
+            // ที่ตัวแปร result
+            //มีคุณสมบัตชื่อ Text
+            result.Text = iResult.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //ข้อความตัวอักษร
+            string inputNum1 = num1.Text;
+            string inputNum2 = num2.Text;
+
+            // convert string to number (interger)
+            // int => interger เลขจำนวนเต็ม
+            // double / float => เลขทศนิยม
+            double iNum1 = double.Parse(inputNum1);
+            double iNum2 = double.Parse(inputNum2);
+
+            // int ทำให้เราสามารถทำการ + - * / ได้
+            double iResult = iNum1 * iNum2;
+
+            // ที่ตัวแปร result
+            //มีคุณสมบัตชื่อ Text
+            result.Text = iResult.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //ข้อความตัวอักษร
+            string inputNum1 = num1.Text;
+            string inputNum2 = num2.Text;
+
+            // convert string to number (interger)
+            // int => interger เลขจำนวนเต็ม
+            // double / float => เลขทศนิยม
+            double iNum1 = double.Parse(inputNum1);
+            double iNum2 = double.Parse(inputNum2);
+
+            // int ทำให้เราสามารถทำการ + - * / ได้
+            double iResult = iNum1 / iNum2;
+
+            // ที่ตัวแปร result
+            //มีคุณสมบัตชื่อ Text
+            result.Text = iResult.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            num1.Text = "";
+            num2.Text = "";
+            result.Text = "";
+        }
+    }
+}
